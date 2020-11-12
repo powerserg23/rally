7bbae3555eb0ad86e1e2a0df962a06259f51591a
Sergio Dejo <sdejoame@uci.edu>
2020-11-11 18:29:18 -0800

    made various layouts

---
+++ app/src/main/java/com/example/rally/Login.java
@@ -0,0 +1,13 @@
+package com.example.rally;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+public class Login extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+    }
+}
