2ff081431454543a604db7de26fad3485520537e
Sergio Dejo <sdejoame@uci.edu>
2020-11-11 17:33:57 -0800

    made various layouts

---
+++ app/src/main/java/com/example/rally/Signup.java
@@ -0,0 +1,15 @@
+package com.example.rally;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+public class Signup extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_page);
+    }
+}
